3ae84c3d20413a491feb7ba9d02ebf0d5f5ee1c8
name <Brunogoat666@gmail.com>
2019-05-22 01:41:42 +0300

    glpjs

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,24 @@ var server = require("browser-sync").create();
 var del = require("del");
 
 
+gulp.task("clean", function () {
+  return del("build");
+});
+
+
+gulp.task("copy", function(){
+  return gulp.src([
+    "source/*.html",
+    "source/fonts/**/*.{woff, woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+
+  ],{
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -30,7 +48,7 @@ gulp.task("css", function () {
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -38,7 +56,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("build/*.html").on("change", server.reload);
+  gulp.watch("source/*.html").on("change", server.reload);
 });
 
 gulp.task("start", gulp.series("css", "server"));
@@ -56,22 +74,5 @@ gulp.task("images", function () {
 .pipe(gulp.dest("build/img"))
 });
 
-gulp.task("build", gulp.series("css"));
+gulp.task("build", gulp.series("clean", "copy", "css"));
 gulp.task("start", gulp.series("build","server"));
-
-
-gulp.task("copy", function(){
-  return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source/*.iso"
-  ],{
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
-})
-
-gulp.task("clean", function () {
-  return del("build");
-});
