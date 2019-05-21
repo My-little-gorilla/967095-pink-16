d0bbd0e35a2b890cd77e21de3cdc9addaf707c19
name <Brunogoat666@gmail.com>
2019-05-21 10:23:54 +0300

    aut

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
 var server = require("browser-sync").create();
+var del = require("del");
+
+
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,8 +21,10 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
@@ -31,7 +38,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("build/*.html").on("change", server.reload);
 });
 
 gulp.task("start", gulp.series("css", "server"));
@@ -46,5 +53,25 @@ gulp.task("images", function () {
     imagemin.jpegtran({progressive: true}),
     imagemin.svgo()
 ]))
-.pipe(gulp.dest("source/img"))
+.pipe(gulp.dest("build/img"))
+});
+
+gulp.task("build", gulp.series("css"));
+gulp.task("start", gulp.series("build","server"));
+
+
+gulp.task("copy", function(){
+  return gulp.src([
+    "source/fonts/**/*.{woff, woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.iso"
+  ],{
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+})
+
+gulp.task("clean", function () {
+  return del("build");
 });
