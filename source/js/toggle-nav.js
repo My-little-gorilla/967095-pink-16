67a52a1e105690cac69523a21d4f1fb9a2240f2e
name <Brunogoat666@gmail.com>
2019-05-23 22:26:02 +0300

    js

--- source/js/toggle-nav.js
+++ source/js/toggle-nav.js
@@ -11,14 +11,16 @@ navToggle.addEventListener('click', function() {
     header.classList.remove('header--closed');
     header.classList.add('header--open');
 
-    navButton.classList.remove('header-navigation__trigger-pic--open');
-    navButton.classList.add('header-navigation__trigger-pic--closed');
+    // navButton.classList.add('header-navigation__trigger-pic--open');
+    navButton.classList.remove('header-navigation__trigger-pic--closed');
   } else {
     menu.classList.add('navigation-list--closed');
     menu.classList.remove('navigation-list--active');
     header.classList.remove('header--open');
     header.classList.add('header--close');
-    navButton.classList.remove('header-navigation__trigger-pic--closed');
-    navButton.classList.add('header-navigation__trigger-pic--open');
+    navButton.classList.add('header-navigation__trigger-pic--closed');
+    navButton.classList.remove('header-navigation__trigger-pic--open');
   }
 });
+navToggle.addEventListener('click', function() {
+}
