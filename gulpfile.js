59b1fdbebb86d8e14fa71df3ebcdb6a682e83141
name <Brunogoat666@gmail.com>
2019-05-29 02:54:07 +0300

    zzzzzzz

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var svgstore = require("gulp-svgstore");
 var cheerio = require("gulp-cheerio");
+var webp = require("gulp-webp");
 
 gulp.task("clean", function () {
   return del("build");
@@ -97,6 +98,12 @@ gulp.task("images", function () {
 ]))
 .pipe(gulp.dest("build/img"))
 });
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
 
 gulp.task("build", gulp.series("clean","sprite","html","css","images","copy"));
 gulp.task("start", gulp.series("build","server"));
